fc375f9c72020ed5bc7f67da53df96cc11a48a00
jycurien <curien.jeanyves@gmail.com>
2023-03-06 14:38:56 +0100

    add unique for article title and slug

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -14,10 +14,10 @@ class Article
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $title = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $slug = null;
 
     #[ORM\Column(type: Types::TEXT)]
