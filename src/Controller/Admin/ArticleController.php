e432ac24a932e3ec2065f35acae130efa1212f1d
jycurien <curien.jeanyves@gmail.com>
2023-03-06 14:19:22 +0100

    finish admin CRUD for article

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +14,12 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(path: '/admin/articles', name: 'admin_article_')]
 class ArticleController extends AbstractController
 {
+    #[Route(path: '/', name: 'index', methods: ['GET'])]
+    public function index(ArticleRepository $repository)
+    {
+        return $this->render('admin/article/index.html.twig', ['articles' => $repository->findLatest()]);
+    }
+
     #[Route(path: '/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -30,5 +37,35 @@ class ArticleController extends AbstractController
         return $this->render('admin/article/new.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    #[Route(path: '/{slug}', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager, Article $article)
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('article_show', ['slug' => $article->getSlug()]);
+        }
+
+        return $this->render('admin/article/edit.html.twig', [
+            'form' => $form,
+            'article' => $article,
+        ]);
+    }
+
+    #[Route(path: '/{slug}', name: 'delete', methods: ['DELETE'])]
+    public function delete(Request $request, EntityManagerInterface $entityManager, Article $article)
+    {
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('delete-article', $submittedToken)) {
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_article_index');
     }
 }
